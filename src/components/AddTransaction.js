b5c800336150093a6b5538c8fc0788edd72e6622
BritainSmith <41024969+BritainSmith@users.noreply.github.com>
2021-08-02 14:37:06 -0500

    Added global state with context

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 
 export const AddTransaction = () => {
-
+    const[text, setText]= useState('');
+    const [amount, setAmount] = useState(0);
 
     return(
         <>
         <h3>Add new Transaction</h3>
         <form >
             <div className="form-control">
-                <label htmlfor="text">Text</label>
-                <input type="text"  placeholder= "Enter text..."/>
+                <label htmlFor="text">Text</label>
+                <input 
+                    type="text" 
+                    value={text} 
+                    onChange={(e)=>{setText(e.target.value)}} 
+                    placeholder= "Enter text..."
+                    />
             </div>
             <div className="form-control">
-                <label htmlfor="amount">
+                <label htmlFor="amount">
                     Amount <br />
                     (negative - expense, positive - income)
                 </label>
-                <input type="number" placeholder="Enter amount ..."/>
+                <input 
+                type="number" 
+                value={amount} onChange={(e)=>setAmount(e.target.value)}
+                placeholder="Enter amount ..."
+                />
 
             </div>
             <button className="btn">Add transaction</button>
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -6,7 +6,7 @@ export const TransactionList = () =>{
     return (
         <>
             <h3>History</h3>
-            <ul id="List" className="list">
+            <ul  className="list">
             <li className="minus">
                 Cash <span>-$400</span>
                 <button className="delete-btn">x</button>
