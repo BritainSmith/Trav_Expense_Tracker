6626b2e417ab79cf4947bf5c90dcf0d88610c116
BritainSmith <41024969+BritainSmith@users.noreply.github.com>
2021-08-02 14:11:43 -0500

    added base css and components

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+
+export const AddTransaction = () => {
+
+
+    return(
+        <>
+        <h3>Add new Transaction</h3>
+        <form >
+            <div className="form-control">
+                <label htmlfor="text">Text</label>
+                <input type="text"  placeholder= "Enter text..."/>
+            </div>
+            <div className="form-control">
+                <label htmlfor="amount">
+                    Amount <br />
+                    (negative - expense, positive - income)
+                </label>
+                <input type="number" placeholder="Enter amount ..."/>
+
+            </div>
+            <button className="btn">Add transaction</button>
+        </form>
+        </>
+
+    )
+}
