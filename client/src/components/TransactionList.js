73a9789e79f71000397e1d3898273d9b92d1adc2
BritainSmith <41024969+BritainSmith@users.noreply.github.com>
2021-08-23 10:35:54 -0500

    updated global state, added transactions

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,25 +1,23 @@
-import React, {useContext} from 'react';
-import {GlobalContext} from '../context/GlobalState';
-import {Transaction} from './Transaction';
+import React, { useContext, useEffect } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Transaction } from "./Transaction";
 
+export const TransactionList = () => {
+  const { transactions, getTransactions } = useContext(GlobalContext);
 
-export const TransactionList = () =>{
+  useEffect(() => {
+    getTransactions();
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const {transactions} = useContext(GlobalContext);
-
-
-
-    return (
-        <>
-            <h3>History</h3>
-            <ul  className="list">
-                {transactions.map(transaction =>(
-
-               <Transaction
-                key={transaction.id} 
-                transaction={transaction}/> ))}
-            
-            </ul>
-        </>
-    )
-}
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
+    </>
+  );
+};
--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,45 +1,67 @@
-import React, {createContext, useReducer} from 'react';
-import AppReducer from './AppReducer';
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+import axios from "axios";
 
 // Initial state
 
-const initialState ={
-
-    transactions: [
-    ]
-
-}
+const initialState = {
+  transactions: [],
+  error: null,
+  loading: true,
+};
 
 //Create context
 
 export const GlobalContext = createContext(initialState);
 
 // Provider component
-export const GlobalProvider = ({children}) =>{
-    const [state, dispatch] =useReducer(AppReducer, initialState);
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    // Actions
-    function deleteTransaction(id) {
-        dispatch({
-            type: 'DELETE_TRANSACTION',
-            payload: id
-        });
+  // Actions
+
+  async function getTransactions() {
+    try {
+      const res = await axios.get("/api/v1/transactions");
+
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: err.response.data.error,
+      });
     }
+  }
 
-    function addTransaction(transaction) {
-        dispatch({
-            type: 'ADD_TRANSACTION',
-            payload: transaction
-        });
-    }
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
 
-    return (
-        <GlobalContext.Provider value={{
-            transactions : state.transactions,
-            deleteTransaction,
-            addTransaction
-        }}>
-        {children}
-        </GlobalContext.Provider>
-    )
-}
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
+        getTransactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
